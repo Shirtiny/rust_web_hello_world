f5a22115a29b77a6a72ceb710752b45a41378245
Shirtiny <49592759+Shirtiny@users.noreply.github.com>
2022-01-12 17:15:49 +0000

    read input

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,21 @@
  * @LastEditTime: 2022-01-06 11:05:59
  * @Description:
  */
+use std::io;
 
-fn main() {
+fn read_input() {
+    let mut input = String::new();
+    println!("input something");
+    match io ::stdin().read_line(&mut input) {
+        Ok(_) => {
+            println!("hello, {}", input)
+        }
+        _ => {}
+    }
+    
+}
+
+fn _prints() {
     println!("{} days {}", 31, 31i64);
     println!("{0}, this is {1}. {1}, this is {0}", "A", "B");
     println!("{a}, this is {b}. {b}, this is {a}", a = "Alice", b = "Bob");
@@ -14,4 +27,10 @@ fn main() {
         "{} of {:b} people know binary, the other half doesn't",
         1, 2
     );
+}
+
+fn main() {
+    read_input();
+    // prints()
+  
 }
